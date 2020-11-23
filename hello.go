5415496df0c735615d20b20e23632c714c60fca0
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-23 13:41:58 -0500

    updating go code

--- hello.go
+++ hello.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello World")
+   var x float64 = 20.0
+
+   y := 42 
+   fmt.Println(x)
+   fmt.Println(y)
+   fmt.Printf("x is of type %T\n", x)
+   fmt.Printf("y is of type %T\n", y)	
 }
+
