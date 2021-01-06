24e8188dac39716c61b93010443a6638cf6a30ea
uonyekwuluje <uionyekwuluje@gmail.com>
2021-01-06 07:40:01 -0500

    testing remote modules

---
+++ go_module/driver.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/uonyekwuluje/golang-development/go_module/src"
+)
+
+func main() {
+  fmt.Printf("Testing My Modules \n")
+  mathops.AddNums()
+}
---
+++ go_module/src/addnum.go
@@ -0,0 +1,11 @@
+package mathops
+
+import (
+  "fmt"
+)
+
+func AddNums() {
+  num1 := 3
+  num2 := 4
+  fmt.Printf("%d + %d = %d\n",num1,num2,(num1+num2))
+}
