ff6b852c8fe726156121676a6990837f9cb86e1e
uonyekwuluje <uionyekwuluje@gmail.com>
2021-01-11 07:22:08 -0500

    updating tests

---
+++ unit_tests/math1.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+  Sum(5,5)
+}
+
+func Sum(x, y int) int {
+  return x + y
+}
---
+++ unit_tests/math1_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "testing"
+  "fmt"
+)
+
+func TestSum(t *testing.T){
+  total := Sum(50,5)
+  if total != 55 {
+    t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+  }
+}
+
+func TestSumTableDriven(t *testing.T) {
+    var tests = []struct {
+        a, b int
+        sum int
+    }{
+        {0, 1, 1},
+        {2, 2, 4},
+        {2, -2, 1},
+        {10, 10, 20},
+        {15, 2, 17},
+    }
+
+    for _, tt := range tests {
+        testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+        t.Run(testname, func(t *testing.T) {
+            ans := Sum(tt.a, tt.b)
+            if ans != tt.sum {
+                t.Errorf("got %d, want %d", ans, tt.sum)
+            }
+        })
+    }
+}
