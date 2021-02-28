a04d0b6afe5935584faf66489955e47e1758562e
uonyekwuluje <uionyekwuluje@gmail.com>
2021-02-28 04:27:36 -0500

    updating algorithms

--- datastructure_algorithms/reverse_integer/main.go
+++ datastructure_algorithms/reverse_integer/main.go
@@ -7,12 +7,13 @@ import (
 func main() {
   var num int = -6792367830
   fmt.Println("Integer =>",num)
-  fmt.Println("Reverse =>",reverse(num))
+  fmt.Println("Reverse1 =>",reverse1(num))
+  fmt.Println("Reverse2 =>",reverse2(num))
 }
 
 
 /* Reverse Integer */
-func reverse(x int) int{
+func reverse1(x int) int{
   var MaxInt int32 = 2147483647
   if x > int(MaxInt) || x < - int(MaxInt) { return 0 }
 
@@ -30,4 +31,23 @@ func reverse(x int) int{
   }
 
   return new_int
+}
+
+func reverse2(x int) int {
+	k := 1
+	limit := 2147483648
+	if x < 0 {
+		k = -1
+		x *= k
+	}
+
+	reverse := 0
+	for x > 0 {
+		reverse = (reverse * 10) +  x % 10
+		x /= 10
+	}
+	if reverse > limit || reverse < -limit {
+		return 0
+	}
+	return reverse * k
 }
