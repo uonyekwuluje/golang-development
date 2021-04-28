86b7e24aa59573f830762ad4dd80e1081239896e
uonyekwuluje <uionyekwuluje@gmail.com>
2021-04-28 17:50:25 -0400

    update test slack

---
+++ projects/slack-bot/snippets/README.md
@@ -0,0 +1,5 @@
+### Bot Links
+https://golangdocs.com/golang-create-your-own-slack-bot
+https://dev.to/shindakun/a-simple-slack-bot-in-go---the-bot-4olg
+
+
---
+++ projects/slack-bot/snippets/send-message.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "log"
+
+    "github.com/slack-go/slack"
+)
+
+func main() {
+    OAUTH_TOKEN := "OAUTH_TOKEN" // Paste your bot user token here
+    CHANNEL_ID := "CHANNEL_ID" // Paste your channel id here
+
+    api := slack.New(OAUTH_TOKEN)
+    attachment := slack.Attachment{
+        Pretext: "Pretext",
+        Text:    "Hello from GolangDocs!",
+    }
+
+    channelId, timestamp, err := api.PostMessage(
+        CHANNEL_ID,
+        slack.MsgOptionText("This is the main message", false),
+        slack.MsgOptionAttachments(attachment),
+        slack.MsgOptionAsUser(true),
+    )
+
+    if err != nil {
+        log.Fatalf("%s\n", err)
+    }
+
+    log.Printf("Message successfully sent to Channel %s at %s\n", channelId, timestamp)
+}
