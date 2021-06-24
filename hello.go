45dec36e6ee0aa9cf0d470c22b085a50d9ffd89b
uonyekwuluje <uionyekwuluje@gmail.com>
2021-06-24 11:57:06 -0400

    build workflow

--- hello.go
+++ hello.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
-  fmt.Println("Test Hello World")
-  fmt.Println("----------------")
+   var n1,n2 string
+   n1 = "Hello"
+   n2 = "World"
+   fmt.Println("String Lenght for ",n1," and ",n2," => ",len(n1))
+   fmt.Println(len(n1) == len(n2))
 }
