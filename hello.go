e224f6d086205759c50c2aa997dac7fc7af70c1d
uonyekwuluje <uionyekwuluje@gmail.com>
2021-07-01 02:12:35 -0400

    update hello

--- hello.go
+++ hello.go
@@ -2,13 +2,9 @@ package main
 
 import (
   "fmt"
+  "strings"
 )
 
 func main() {
-   xs := []int{4,6,8,2,8}
-   total := 0
-   for _, v := range xs {
-      total += v
-   }
-  fmt.Println("Total => ",total)
+   fmt.Println(strings.Contains("test","es"))
 }
