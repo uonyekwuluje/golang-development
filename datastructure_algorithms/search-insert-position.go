784d1187b3ed2bbd3789847fd908a38ef850d441
uonyekwuluje <uionyekwuluje@gmail.com>
2021-07-09 07:28:49 -0400

    commit code changes

--- datastructure_algorithms/search-insert-position.go
+++ datastructure_algorithms/search-insert-position.go
@@ -5,15 +5,28 @@ import (
 )
 
 func main() {
-  numArray := []int{1,3,5,6}
-  num := 5
-  searchInsert(numArray,num)
+  numArray := []int{1,3}
+  num := 3
+  //numArray := []int{1,3,5,6}
+  //num := 0
+  fmt.Println(searchInsert(numArray,num))
 }
 
 
 
-//func searchInsert(nums []int, target int) int {
-func searchInsert(nums []int, target int) {
-    fmt.Println("Numbers =>",nums)
-    fmt.Println("Target =>",target)
+func searchInsert(nums []int, target int) int {
+   var found int
+   for x:=0; x<len(nums); x++ {
+     if (target == nums[x]) {
+         found = x
+         break
+     } else if (target < nums[x]) {
+         found = x
+         break
+     } else if (target > nums[len(nums)-1]) {
+         found = len(nums)
+         break
+     }
+   }
+  return found
 }
