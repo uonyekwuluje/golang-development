810e586afb5b3f5fddcdc75499f1a20004f87aeb
uonyekwuluje <uionyekwuluje@gmail.com>
2021-07-16 18:19:49 -0400

    testing function

--- projects/intro/intro.go
+++ projects/intro/intro.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-  "fmt"
-)
-
 func Sum(x int, y int) int {
   return x + y
 }
 
 func main() {
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
   Sum(5,5)
 }
